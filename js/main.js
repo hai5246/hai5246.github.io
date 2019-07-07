2355e870b27eeee7193332180e653b9de3f0bd65
hai5246 <972026328@qq.com>
2019-07-07 22:52:07 +0800

    Site updated: 2019-07-07 22:52:06

--- js/main.js
+++ js/main.js
@@ -242,25 +242,25 @@ $(function () {
   }
   //---------------------------------------------------------------------------------------------------
   //fancybox
-  var imgList = $(".recent-post-info  img").not('.no-fancybox');
+  var imgList = $(".recent-post-info  img");
   if (imgList.length === 0) {
-    imgList = $("#post-content img").not('.no-fancybox');
+    imgList = $("#post-content img");
   }
-  
   for (var i = 0; i < imgList.length; i++) {
     var $a = $(
       '<a href="' +
-        imgList[i].src +
-        '" data-fancybox="group" data-caption="' +
-        imgList[i].alt +
-        '" class="fancybox"></a>'
-    )
-    var alt = imgList[i].alt
-    var $wrap = $(imgList[i]).wrap($a)
+      imgList[i].src +
+      '" data-fancybox="group" data-caption="' +
+      imgList[i].alt +
+      '" class="fancybox"></a>'
+    );
+    var alt = imgList[i].alt;
+    var $wrap = $(imgList[i]).wrap($a);
     if (alt) {
-      $wrap.after('<div class="img-alt">' + alt + '</div>')
+      $wrap.after('<div class="img-alt">' + alt + "</div>");
     }
   }
+
   $().fancybox({
     selector: "[data-fancybox]",
     loop: true,
